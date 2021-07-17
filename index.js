cb8dbf3dece4d7473953e98e21ebbc19cf843726
Nicole Ampofo <30731402+nicoleampofo@users.noreply.github.com>
2021-07-17 19:08:31 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+// findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+// findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+// It’s guaranteed that array contains at least 3 numbers.
+
+function findUniq(arr){
+  arr.sort((a,b)=>a-b)
+  return arr[0] === arr[1] ? arr.pop() : arr[0]
+}
+
+// Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
+
+// Don't change the order of the elements that are left.
+
+// Examples
+// removeSmallest([1,2,3,4,5]) = [2,3,4,5]
+// removeSmallest([5,3,2,1,4]) = [5,3,2,4]
+// removeSmallest([2,2,1,2,1]) = [2,2,2,1]
+
+
+function removeSmallest(numbers){
+  let indexOfMin = numbers.indexOf(Math.min(...numbers));
+  return [...numbers.slice(0, indexOfMin), ...numbers.slice(indexOfMin + 1)];
+}
